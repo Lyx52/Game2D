27a4a55d5cc3660b772f38e8e9149cdcc34bf75d
Ļyx52 <lyxikars123@gmail.com>
2021-10-22 13:25:15 +0300

    Add todo's

--- Game.Entity/Components/OrthoCamera.cs
+++ Game.Entity/Components/OrthoCamera.cs
@@ -3,21 +3,39 @@ using Game.Utils;
 using System;
 
 namespace Game.Entity {
+    public struct CameraQuad {
+        public Vector2 Min;
+        public Vector2 Max;
+
+        public CameraQuad(float left, float right, float bottom, float top) {
+            this.Min = new Vector2(left, top);
+            this.Max = new Vector2(right, bottom);
+        }
+        public void SetQuad(float left, float right, float bottom, float top) {
+            this.Min.X = left;
+            this.Min.Y = top;
+            this.Max.X = right;
+            this.Max.Y = bottom;
+        }
+    } 
     public class OrthoCamera {
         private Matrix4 projection;
         private Matrix4 view;
         public Matrix4 ViewProjection { get; set; }
         public double Rotation { get; set; }
         public IntPtr PtrViewProjection { get; set; }
+        public CameraQuad CameraQuad;
         public OrthoCamera(float left, float right, float bottom, float top) {
-            this.projection = Matrix4.CreateOrthographicOffCenter(left, right, bottom, top, 3.0f, -3.0f);
+            this.projection = Matrix4.CreateOrthographicOffCenter(left, right, bottom, top, 1.0f, -1.0f);
+            this.CameraQuad = new CameraQuad(left, right, bottom, top);
             this.view = Matrix4.Identity;
             this.Rotation = 0;
             this.ViewProjection = view * projection;
             this.PtrViewProjection = IOUtils.GetObjectPtr(this.ViewProjection);
         }
         public void SetProjection(float left, float right, float bottom, float top) {
-            this.projection = Matrix4.CreateOrthographicOffCenter(left, right, bottom, top, 3.0f, -3.0f);
+            this.projection = Matrix4.CreateOrthographicOffCenter(left, right, bottom, top, 1.0f, -1.0f);
+            this.CameraQuad.SetQuad(left, right, bottom, top);
             this.ViewProjection = view * projection;
         }
         public void Recalculate(in Vector2 position) {
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Game2D
 Simple c# and opentk 2d game
+
+TODO:
+* Animations
+* AABB Collisions
+* World/Chunk/Tile system
+* Infinite tilemaps
+* Simple A* Path finding
+* World serialization, and world streaming
